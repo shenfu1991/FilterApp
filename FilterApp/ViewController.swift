f9ad165ad6b6704261af1c38095cb76bc6e123b2
xuanyuan <xuanyuanshenfu@outlook.com>
2021-11-14 22:22:51 +0800

    Initial Commit

---
+++ FilterApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FilterApp
+//
+//  Created by xuanyuan on 2021/11/14.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
